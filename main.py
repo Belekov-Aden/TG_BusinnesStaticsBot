68f03cf607e4b6590dec7e371b3722224428d24a
B.Aden <belekovaden31@gmail.com>
2025-01-13 14:34:20 +1100

    add bots in account, and read message done, then create static for single user saves static

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
+import asyncio
 import os
 import logging
-
-
-from openai import OpenAI
+import sys
 from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 load_dotenv()
 
-client = OpenAI(
-    api_key=os.getenv("OPENAI_KEY")
-)
+TOKEN = os.getenv("SECRET_KEY")
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    store=True,
-    messages=[
-        {"role": "user", "content": "write a haiku about ai"}
-    ]
-)
+dp = Dispatcher()
 
-print(completion.choices[0].message)
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.business_message()
+async def read_handler(message: Message) -> None:
+    print(f'User: {message.from_user.username}\nText: {message.text}')
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
