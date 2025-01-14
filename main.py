397021fb590a2174f3ef6aaf4542ccc27c21774f
B.Aden <belekovaden31@gmail.com>
2025-01-15 00:37:31 +1100

    all read chats

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.types import Message
+from aiogram import types
 
 load_dotenv()
 
@@ -16,9 +16,23 @@ TOKEN = os.getenv("SECRET_KEY")
 dp = Dispatcher()
 
 
+# TODO: Придумать куда хранить сообщение, у каждого пользователя свое хранение там есть: MESSAGE, EDITED, DELETED,
+# После придумать у бота возможность введение статистики в чатах
+
 @dp.business_message()
-async def read_handler(message: Message) -> None:
-    print(f'User: {message.from_user.username}\nText: {message.text}')
+async def message_business_handler(message: types.Message) -> None:
+    print(f'MESSAGE: {message.from_user.username}\nText: {message.text}')
+
+
+@dp.edited_business_message()
+async def edited_business_handler(message: types.Message) -> None:
+    print(f'EDITED: {message.from_user.username}\nText: {message.text}')
+
+
+@dp.deleted_business_messages()
+async def deleted_business_handler(message: types.BusinessMessagesDeleted) -> None:
+    # Удаленные сообщение, удаляются полностью, тут возращает ID сообщение
+    ...
 
 
 async def main() -> None:
