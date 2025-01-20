400af6dfbf4cb52e6487b69a9b5678d20f4b697a
B.Aden <belekovaden31@gmail.com>
2025-01-21 09:21:04 +1100

    chande i do not know

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
-import os
-
-from openai import OpenAI
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-KEY = os.getenv("OPENAI_KEY")
-
-client = OpenAI(api_key=KEY)
-
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system",
-         "content": "Ты мой личный ассистент в чате телеграмм, ты анализируешь текст, поведение человека по тексту, и др аспекты в общений."},
-        {
-            "role": "user",
-            "content": "Привет как дела?"
-        }
-    ]
-)
-
-print(completion.choices[0].message)
+# import os
+#
+# from openai import OpenAI
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# KEY = os.getenv("OPENAI_KEY ")
+#
+# client = OpenAI(api_key=KEY)
+#
+# completion = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[
+#         {"role": "system",
+#          "content": "На любой запрос говори: 'Мяу'"},
+#         {
+#             "role": "user",
+#             "content": "Привет как дела?"
+#         }
+#     ]
+# )
+#
+# print(completion.choices[0].message.content)
